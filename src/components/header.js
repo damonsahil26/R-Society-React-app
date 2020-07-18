043e8a22247b3ad732cfc150416e37023cfad12f
Sahil Malhotra <sahil061626@gmail.com>
2020-07-18 09:56:43 +0530

    Add files via upload
    Initial-Commit

---
+++ src/components/header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+function Header(){
+    
+    return <div>
+        <header className="header">
+        <div className="logo-box">
+            <img className="logo" src ="./../images/logo-white.png" alt="logo"></img>            
+        </div>
+        <div class=
+        "text-box">
+    <h1 class="heading-primary">
+    <span class="heading-primary-main">R:SOCIETY</span>
+    <span class="heading-primary-sub">You can make a change</span>
+    </h1>
+    </div>
+    </header>
+  
+    </div>;
+}
+export default Header;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from "./components/header"
+
+ReactDOM.render(
+  <div>
+<Header />
+</div>,
+  document.getElementById('root')
+);
+
+
